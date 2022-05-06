664311e5cc178c1a0cb19c427a49fb9306adf2d6
u0kn <local@dev>
2022-05-07 05:19:08 +0800

    change version

--- Soulworker Utility/UI/PlayerTable.cpp
+++ Soulworker Utility/UI/PlayerTable.cpp
@@ -89,7 +89,7 @@ VOID PlayerTable::Update() {
 
 		CHAR title[128] = { 0 };
 
-		sprintf_s(title, 128, "%s : %02d:%02d [v1.2.9.8_%s]  ###DamageMeter", DAMAGEMETER.GetWorldName(), (UINT)DAMAGEMETER.GetTime() / 60, (UINT)DAMAGEMETER.GetTime() % 60, SWPACKETMAKER.GetKeyInfo());
+		sprintf_s(title, 128, "%s : %02d:%02d [v1.2.9.9_%s]  ###DamageMeter", DAMAGEMETER.GetWorldName(), (UINT)DAMAGEMETER.GetTime() / 60, (UINT)DAMAGEMETER.GetTime() % 60, SWPACKETMAKER.GetKeyInfo());
 		ImGui::Begin(title, 0, windowFlag);
 		{
 			if (!UIOPTION.isOption() || _tableResize)
