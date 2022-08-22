490e419f9a69b60383a1d11ad1de59a500b3ae46
u0kn <local@dev>
2022-08-23 02:07:10 +0800

    Fix stat ignore bug

--- Soulworker Utility/Soulworker Packet/SWPacketStatChange.cpp
+++ Soulworker Utility/Soulworker Packet/SWPacketStatChange.cpp
@@ -36,15 +36,16 @@ VOID SWPacketStatChange::Do() {
 #endif
 			continue;
 		}
+		else {
+			//Log::MyLog(_T("[DEBUG] [ID %08x] [statType = %x], [statValue = %f]\n"), stat_header->_playerID, party_data->_statType, party_data->_statValue);
+			DAMAGEMETER.UpdateStat(stat_header->_playerID, party_data->_statType, party_data->_statValue);
 
-		//Log::MyLog(_T("[DEBUG] [ID %08x] [statType = %x], [statValue = %f]\n"), stat_header->_playerID, party_data->_statType, party_data->_statValue);
-		DAMAGEMETER.UpdateStat(stat_header->_playerID, party_data->_statType, party_data->_statValue);
-
-		CombatLog* pCombatLog = new CombatLog;
-		pCombatLog->_type = CombatLogType::CHANGED_STATS;
-		pCombatLog->_val1 = party_data->_statType;
-		pCombatLog->_val2 = party_data->_statValue;
-		COMBATMETER.Insert(stat_header->_playerID, CombatType::PLAYER, pCombatLog);
+			CombatLog* pCombatLog = new CombatLog;
+			pCombatLog->_type = CombatLogType::CHANGED_STATS;
+			pCombatLog->_val1 = party_data->_statType;
+			pCombatLog->_val2 = party_data->_statValue;
+			COMBATMETER.Insert(stat_header->_playerID, CombatType::PLAYER, pCombatLog);
+		}
 
 		p_data += sizeof(SWPACKETSTATCHANGE_DATA);
 	}
