146fe0e36430fd368bef671774661d06039af030
neonr-0 <neon-0@outlook.com>
2023-05-04 15:35:14 +0300

    game update

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define APP_VERSION "1.3.5.1"
-#define SWMAGIC 21
+#define APP_VERSION "1.3.5.2"
+#define SWMAGIC 22
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
 #define USE_XOR 0
