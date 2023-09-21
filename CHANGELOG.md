9cfc450a7ce7606fea755c1a9a597fbb24e0ecd6
neonr-0 <neon-0@outlook.com>
2023-09-21 14:01:00 +0300

    Game update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.3.6.2
+
+- Updated meter and database
+
 ## 1.3.6.1
 
 - Updated database, possible fix crash after people leaving squad 
--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define APP_VERSION "1.3.6.1"
-#define SWMAGIC 36
+#define APP_VERSION "1.3.6.2"
+#define SWMAGIC 37
 #define SWCONSTVALUE_RECV 1
 #define SWCONSTVALUE_SEND 2 // maybe client send request
 #define USE_XOR 0
